fa94646f6a3eacb16dea9fa9e5fcd2d618f04de9
AhmedBoukhousse <47191893+AhmedBoukhousse@users.noreply.github.com>
2020-10-02 18:36:33 -0500

    Update Player.h
    Finished up the Player class

--- Player.h
+++ Player.h
@@ -18,7 +18,28 @@ class Player{
       int getPlayerId();
       int getPiecesLeft();
       int getPiecesDisplayed();
-      int setPieces();
+      int <int> getPiecesDisplayedVector();
+      
+      // setter functions
+      void increasePieces();
+      
+      
+      //Update the state of the pieces displayed on board
+      void displayPiecesOnBoard(int position);
+      void removePieces(int position);
+      
+      //This function should be called if the oppoent forms a Mill. 
+      //it checks if the player has any pieces that are volunerable and can be removed. 
+      //if no pieces are vulnerable then the opponet will not be allowed to remove any pieces
+      // return true if the player's pieces on board meet the requirement to be removed (not in a Mill or in a mill but the last 3 pieces)
+      bool hasVolunerablePieces(vector<int> protectedPieces);
+      
+      /* the hasNoPieces checks if a certain player is out of pieces 
+      */
+      bool hasNoPieces();
+      
+      // reset the game if the restart button is pressed
+      void reset();
     
 };
-#endif
+#endif //PLAYER
