8e77e00c83b0fd3e288bf1bccb133c79678dfa15
OI2345-der <71655271+OI2345-der@users.noreply.github.com>
2020-10-02 21:28:20 -0500

    Add files via upload

--- move.cpp
+++ move.cpp
@@ -3,12 +3,16 @@
 
 using namespace std;
 
-char taken = 'X';
+int p1 = 1;
+int p2 = 2;
 
 bool legalmoves(int newlocation) // check location selected by the user
 {
 	
-	if (newlocation == taken) {  // if it's taken
+	if (newlocation == p1) {  // if it's taken
+		return false;
+	}
+	else if (newlocation == p2) {
 		return false;
 	}
 	else
@@ -18,7 +22,7 @@ bool legalmoves(int newlocation) // check location selected by the user
 void movepiece(int prevlocation, int newlocation) // move piece to legal position
 {
 	if (legalmoves(newlocation) == true) {
-		newlocation = taken;			// piece moved to new location
+		prevlocation = newlocation;			// piece moved to new location
 	}
 	else
 		cout << "invalid move" << endl;
