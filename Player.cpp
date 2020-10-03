e388b66804d0c23713391af83d731365cb37e97f
AhmedBoukhousse <47191893+AhmedBoukhousse@users.noreply.github.com>
2020-10-02 19:20:22 -0500

    Update Player.cpp
    Implemented the Player class functions

--- Player.cpp
+++ Player.cpp
@@ -3,7 +3,7 @@
 // argument constructor
 Player::Player(int id)
 {
-  pieces = 9;
+  piecesLeft = 9;
   this-> id = playerID;
 
 }
@@ -20,8 +20,64 @@ int Player::getPiecesLeft()
   return piecesLeft;
 }
 
-int Player::getPiencesDisplayed()
+int Player::getPiecesDisplayed()
 {
   return piecesDisplayed.size();
 
+}
+
+vector<int> Player::getPiecesDisplayedVector()
+{
+  return piecesDisplayed;
+}
+
+void Player::increasePieces()
+{
+  piecesLeft++;
+
+void Player::removePieceFromBoard(int position)
+{
+  
+  if(getPicesDisplayed() > 0)
+  {
+    piecesDosplayed.erase(std::find(piecesDisplayed.begin(), piecesDisplayed.end(), position));
+  }
+  
+}
+  
+ void Player::makePiecesOnBoardMove(int position)
+ {
+   removePieces(position1, position2);
+   piecesLeft++;
+   displayPieceToTheBoard(position 2);
+   
+ }
+  
+  void Player::displayPieceToBoard(int position)
+ {
+    if(piecesLeft > 0)
+    {
+        piecesLeft--;
+      piecesDisplayed.push_back(position);
+    }
+ }
+ 
+  
+bool Player::hasVolunerablePieces(vector<int> protectedPieces)
+{
+  for(vector<int>::iterator it = piecesDisplayed.begin(); it != piecesDisplayed.end(); ++it)
+  {
+    if(std::find(protectedPieces.begin(), protectedPieces.end(), *it) == protectedPieces.end()) return true;
+  }
+  return false;
+}
+bool Player::hasNoPieces()
+{
+  return piecesLeft == 0;
+}
+  
+void Player::clearBoard()
+{
+  piecesDisplayed.clear();
+  piecesLeft = 9;
 }
